4544818c7d309001b7bbf172b80cdd92515e6205
fensoft <fensoft@gmail.com>
2024-08-30 00:38:14 +0200

    fix for macos

--- release.sh
+++ release.sh
@@ -8,7 +8,7 @@ cd portalturret
 git fetch --tags --force
 git checkout $1
 if [ "$OS" = "Windows_NT" ]; then
-  echo $HOME/.platformio/penv/Scripts/pio.exe run
+  $HOME/.platformio/penv/Scripts/pio.exe run
 else
   pio run
 fi
@@ -24,4 +24,12 @@ if [ "$OS" = "Windows_NT" ]; then
   cp icon.png flasher.dist
   rm -rf flasher-$1
   cp -rf flasher.dist flasher-$1
+else
+  SP=`python -m site | grep site-packages | grep -v USER_SITE | sed "s#^[ ]*'##g" | sed "s#',\\$##"`
+  ls $SP/esptool/targets/stub_flasher
+  mkdir -p flasher.app/Contents/MacOS/esptool/targets/stub_flasher
+  cp $SP/esptool/targets/stub_flasher/* flasher.app/Contents/MacOS/esptool/targets/stub_flasher
+  cp icon.png flasher.app/Contents/MacOS/
+  rm -rf flasher-$1-$MACHTYPE.app
+  cp -rf flasher.app flasher-$1-$MACHTYPE.app
 fi
